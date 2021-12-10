81c1e958204bd4fcd7b65206ae56ced940c542e8
Arthur <arthur.saunier2@gmail.com>
2021-12-10 10:52:29 +0100

    initial commit

---
+++ microbit/Gateway - Sensor/main.py
@@ -0,0 +1,39 @@
+from microbit import *
+import radio
+
+radio.config(group=199, address=0x24667818, length=251)
+radio.on()
+
+serial_uart = uart.init(baudrate=115200)
+
+def send_to_gateway(content):
+    uart.write(content)
+
+def send_to_sensors(packet):
+    radio.send(packet)
+
+PACKET_END = "[END]"
+
+LAST_PACKET = b""
+
+while True:
+    # redirect sensor incoming packets to the controller
+    new_log_packet = radio.receive()
+    if new_log_packet != None:
+        send_to_gateway(new_log_packet + PACKET_END)
+
+    # redirect packets from the controller to all sensors
+    request = uart.readline()
+    if request != None:
+        LAST_PACKET += request
+        if PACKET_END in LAST_PACKET:
+            packet = str(LAST_PACKET).replace("b'", "")[:-1].replace(PACKET_END, "")
+            send_to_sensors(packet)
+            print(packet)
+            #request_content = protocol.receive_packet(packet)
+            #print("packet redistributed: ", packet)
+            LAST_PACKET = b""
+            
+            # Send temperature to server through gateway
+            # packet = protocol.send_packet(str(data), 1)
+            # send_to_gateway(packet + "[END]")
---
+++ microbit/Gateway - Sensor/protocol.py
@@ -0,0 +1,122 @@
+import math
+"""
+Encryption
+"""
+class Encryption():
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':.,/-_0123456789{}()\" "
+
+    def __init__(self, key):
+        self.key = key
+    
+    @staticmethod
+    def chr(index: int) -> str:
+        try:
+            return Encryption.alphabet[index]
+        except IndexError:
+            raise Exception("Encyption does not support <>")
+
+    @staticmethod
+    def ord(char: str) -> int:
+        try:
+            return Encryption.alphabet.index(char)
+        except ValueError:
+            raise Exception("Encyption does not support <>")
+
+    def encrypt(self, clear):
+        enc = []
+        for i in range(len(clear)):
+            key_c = self.key[i % len(self.key)]
+            enc_c = Encryption.chr((Encryption.ord(clear[i]) + Encryption.ord(key_c)) % len(Encryption.alphabet))
+            enc.append(enc_c)
+        return "".join(enc)
+
+    def decrypt(self, enc):
+        dec = []
+        for i in range(len(enc)):
+            key_c = self.key[i % len(self.key)]
+            dec_c = Encryption.chr((len(Encryption.alphabet) + Encryption.ord(enc[i]) - Encryption.ord(key_c)) % len(Encryption.alphabet))
+            dec.append(dec_c)
+        return "".join(dec)
+
+"""
+Packet
+"""
+class Packet():
+    def __init__(self, from_addr: int, to_addr: int, encoded_message: str):
+        self.from_addr = from_addr
+        self.to_addr = to_addr
+        self.encoded_message = encoded_message
+        self.checksum = 0
+
+        self.compute_checksum()
+
+    def compute_checksum(self):
+        nleft = len(self.encoded_message)
+        sum = 0
+        pos = 0
+        while nleft > 1:
+            sum = ord(self.encoded_message[pos]) * 256 + (ord(self.encoded_message[pos + 1]) + sum)
+            pos = pos + 2
+            nleft = nleft - 2
+        if nleft == 1:
+            sum = sum + ord(self.encoded_message[pos]) * 256
+
+        sum = (sum >> 16) + (sum & 0xFFFF)
+        sum += (sum >> 16)
+        sum = (~sum & 0xFFFF)
+
+        self.checksum = sum
+
+class Format():
+    def dumps(self, packet: Packet) -> str:
+        return "|".join([str(packet.to_addr), str(packet.from_addr), str(packet.encoded_message), str(packet.checksum)])
+
+    def parse(self, string: str) -> Packet:
+        packet = string.split("|")
+        return self.compare_checksum(Packet(int(packet[1]), int(packet[0]), packet[2]), packet[3])
+
+    def compare_checksum(self, compute_packet: Packet, received_checksum: str) -> Packet:
+        return compute_packet if int(received_checksum) == compute_packet.checksum else None
+
+
+
+"""
+Protocol
+"""
+class RadioProtocol:
+    def __init__(self, address: int, encryption: Encryption, format: Format):
+        self.addr = address
+        self.encryption = encryption
+        self.format = format
+        self.group = math.floor(address / 100) * 100
+    
+    def send_packet(self, message, addrDest: int) -> str:
+        packet = Packet(self.addr, addrDest, self.encryption.encrypt(message))
+        return self.format.dumps(packet)
+
+    def receive_packet(self, string_packet: str):
+        if string_packet != None:
+            packet = self.format.parse(string_packet)
+            if packet != None:
+                if self.addr == packet.to_addr or self.group == packet.to_addr:
+                    return self.encryption.decrypt(packet.encoded_message)
+                else:
+                    print("Packet received isn't destined to us !")
+            else:
+                print("Packet received isn't valid !")
+        return None
+
+def escape_attribute(string):
+    return string.replace('"', "").replace("'", "").lstrip()
+
+
+def parse_single_feature_line(attributestring):
+    try:
+        attributestring = attributestring.replace("{", "").replace("}", "")
+        attributes = dict()
+        for keyvaluepair in attributestring.split(','):
+            key,value=keyvaluepair.split(':')
+            attributes[escape_attribute(key)] = escape_attribute(value)
+        return attributes
+    except:
+        return None
