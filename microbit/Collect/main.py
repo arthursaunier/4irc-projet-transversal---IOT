810234bb07e0eb21c8998cf384d7bef7b11869e4
Arthur <arthur.saunier2@gmail.com>
2021-12-15 16:46:46 +0100

    gestion reception uart

--- microbit/Collect/main.py
+++ microbit/Collect/main.py
@@ -6,18 +6,14 @@ radio.config(group=199, address=0x24667818, length=251)
 radio.on()
 
 protocol = RadioProtocol(0)
-
 serial_uart = uart.init(baudrate=115200)
 
 def send_to_gateway(content):
     uart.write(content)
 
-def send_to_sensors(packet):
-    radio.send(packet)
-
 PACKET_END = "[END]"
-
-LAST_PACKET = b""
+LAST_MESSAGE = ""
+ADDRESS_DEST = 100
 
 data = {}
 
@@ -46,28 +42,18 @@ while True:
                 #renvoie un ack au microbit qui a envoyé  le message
                 data["ack"] = 0
                 radio.send(protocol.send_packet(str(data), packet_obj.from_addr))
-
-                #todo 
                 # traitement message
-                send_to_gateway(packet_obj.encoded_message)
-            
-
-                
-        #UART
-        
+                send_to_gateway(packet_obj.encoded_message)        
 
     # redirect packets from the controller to all sensors
-    request = uart.readline()
-    if request != None:
-        LAST_PACKET += request
-        if PACKET_END in LAST_PACKET:
-            packet = str(LAST_PACKET).replace("b'", "")[:-1].replace(PACKET_END, "")
-            send_to_sensors(packet)
-            print(packet)
-            #request_content = protocol.receive_packet(packet)
-            #print("packet redistributed: ", packet)
-            LAST_PACKET = b""
-            
-            # Send temperature to server through gateway
-            # packet = protocol.send_packet(str(data), 1)
-            # send_to_gateway(packet + "[END]")
+    serial = uart.read()
+    if serial != None:
+        LAST_MESSAGE += str(serial).replace("b'", "").replace("'", "")
+
+        radio.send(protocol.send_packet(LAST_MESSAGE, ADDRESS_DEST))
+        LAST_MESSAGE = ""
+        """
+        if PACKET_END in LAST_MESSAGE:
+            uart.write(LAST_MESSAGE)
+            LAST_MESSAGE = ""
+        """
