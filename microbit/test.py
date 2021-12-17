5b12ae7a0f346fa01480beffa2d57cb9da124c3a
Arthur <arthur.saunier2@gmail.com>
2021-12-17 10:57:06 +0100

    test nouvelle version
    version opti microbit

--- microbit/test.py
+++ microbit/test.py
@@ -1,16 +1,9 @@
 from Collect.protocol import RadioProtocol, parse_single_feature_line
 import time
-from Collect.protocol import Encryption, Format
 
-key = "a"
+protocol_envoi = RadioProtocol(0)
 
-truc = Encryption(key)
-form = Format
-
-
-protocol_envoi = RadioProtocol(0, truc, form)
-
-protocol_reception = RadioProtocol(1, truc, form)
+protocol_reception = RadioProtocol(1)
 
 #[{"latitude":45.723967,"intensity":30,"radius":10,"id":"9560c125-ad99-4b54-a08d-19079a7f1ddb","longitude":4.907738,"updatedAt":"2021-12-15T10:15:48.167Z","createdAt":"2021-12-15T10:15:48.167Z"}][END]
 
@@ -25,7 +18,7 @@ if __name__ == "__main__":
     #print("packet env:", paquet)
     
     truc = protocol_reception.receive_packet(str(paquet))
-    recu = parse_single_feature_line(truc)
+    recu = parse_single_feature_line(truc.encoded_message)
 
     #print("packet rec:", truc)
     print("packet traiter", recu)
